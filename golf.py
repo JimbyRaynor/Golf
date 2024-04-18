27802434bf45d054d1c9657b8c62f6155e6e5d8d
Jimby Raynor <67350304+JimbyRaynor@users.noreply.github.com>
2024-04-18 16:20:25 +1000

    Add files via upload

--- golf.py
+++ golf.py
@@ -1,7 +1,10 @@
+import random, math
+
 print("Golf - text version. From the book 101 basic games")
 f= 1 # hole number
 h=0 # handicap
 t=0 # handicap type
+n=0.8 # n used to see if rnd(1) > n handicap works
 s1= 0 # number of shots taken
       # s1=0 means get ready to tee off
 s2=0 # hook when s2 =14, also slice
@@ -13,7 +16,9 @@ p=0 # par (good average) for hole
 c=0 # club number
 w=0 # weighting of swing, 1 is max, 0 is no swing
 d=0 # distance to hole
+d2 = 0 #distance to hole
 L=[0,1,1,1,1,1,1,1] # is the type of terrain
+b=0 # previous distance to cup
 
 #L[]=1 is fairway, 2 is rough,
 #    3 is trees, 4 is adjacent fairway
@@ -32,6 +37,10 @@ holedata=[361,4,4,2,389,4,3,3,206,3,4,2,500,5,7,2,\
 196,3,7,2,400,4,7,2,560,5,7,2,132,3,2,2,\
 357,4,4,4,294,4,2,4,475,5,2,3,375,4,4,2,\
 180,3,6,2,550,5,6,6]
+
+
+def RND():  # random real number between 0 and 1
+    return random.random()
 
 holes=[]
 
@@ -78,9 +87,10 @@ def chooseclub():
         print("That club is not in the bag")
   return c
       
-holefinished = False
+
        
 while (f <= g1) : # f is hole number
+  holefinished = False  
   L[0] = 0  # f loop
   j=0
   q=0
@@ -100,7 +110,7 @@ while (f <= g1) : # f is hole number
   d = holes[f-1]["distance"]
   p = holes[f-1]["par"]
   g3=g3+p
-  print(L)
+  #print(L)
   print("You are at the tee off for Hole",f,\
         ", Distance",d,"yards, par",p)
   print("On your right is")
@@ -116,6 +126,44 @@ while (f <= g1) : # f is hole number
         print("Now gauge your distance by a percentage (1 to 100)")  
         print("of a full swing")
         w = int(input("percentage = "))
+        w = w / 100
         print()
+      if int(f/3) == f/3: #L952
+          if s2+q+(10*(f-1)/18) < (f-1)*(72+1/.85)/18:
+            q=q+1 #L956
+            if s1/2 != int(s1/2):
+                if d < 95:
+                    print("You dubbed it")
+                    d1 = 35   
+      if c < 4 and L[0]==2:
+         print("You dubbed it")
+         d1 = 35
+      else:
+         if s1 > 7: #L760
+             if d < 200: #L867
+                d2 = 1 + 3*int(2*RND())
+     #L770
+      d1 = int(30*2.5+187-(30*.25+15)*c/2)+25*RND()
+      d1 = int(d1*w)
+      #L830
+      # d is original distance to cup
+      # d,d1,d2,o described on line 360
+      # d1 is shot distance
+      # d2 is new distance to cup
+      # o is offline distance on side of course, o=0 when in middle of course
+      o = RND()/.8*16*abs(math.tan(d1*0.0035))
+      d2 = int(math.sqrt(o**2+(d-d1)**2))
+      if (d1 > d) and (d2 >= 20):
+        print("Too much club. You are past the hole.")  
       print("shot", s1)
+      if L[0] == 5:  # trap
+         d2 = 1+(3*int(2*RND()))
+      print("On green, ",d2, "feet from the pin.")
+      i = input("Choose your putt potency (1 to 13): ")
+      i = int(i)
+      s1 = s1 + 1
+      if s1+1 -p > 2:
+          print("You holed it")
+          f=f+1
+          holefinished = True
   f = f + 1
